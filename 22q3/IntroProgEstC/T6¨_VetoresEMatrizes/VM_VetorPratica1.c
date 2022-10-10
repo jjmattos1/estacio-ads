89291e365498b6f0dcf1555201830ade2d818b6c
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-10-10 19:14:07 -0300

    Atualizando repo Windows.

--- 22q3/IntroProgEstC/T6¨_VetoresEMatrizes/VM_VetorPratica1.c
+++ 22q3/IntroProgEstC/T6¨_VetoresEMatrizes/VM_VetorPratica1.c
@@ -8,26 +8,28 @@ int main ()
 	int vetSize = 3;
 	int vetP1[3];
 	int repStart1, repStart2, repStart3, vetNum = 0;
-	/*
-	for ( repStart1 = 0 ; repStart1 >= vetSize ; repStart1 ++ )
+	
+	for ( repStart1 = 0 ; repStart1 <= vetSize ; repStart1 ++ )
 	{
 		vetP1[repStart1] = 0;
 	}
-	*/
-	for ( repStart2 = 0 ; repStart2 >= vetSize ; repStart2 ++ )
+	
+	for ( repStart2 = 0 ; repStart2 <= vetSize ; repStart2 ++ )
 	{	
-		printf("Digite o numero desejado da posicao do Vetor: ", repStart2);
-		scanf("%d", (&vetP1[vetNum]));
+		printf("Digite o numero desejado da posicao %d do Vetor: ", repStart2);
+		scanf("%d", (&vetP1[repStart2]));
+		
 		printf(" \n");
 	}
-	/*
+	
 	for ( repStart3 = vetSize ; repStart3 >= 0 ; repStart3 -- )
 	{	
-	printf("\nO numero da posicao %d do vetor e: ", repStart3);
-		printf(" ");
+		printf("O numero da posicao %d do vetor e: ", repStart3);
+		//printf(" ");
 		printf("%d", (vetP1[repStart3]));
+		printf(" \n");
 	}
-	*/
-	printf(" \n");	
+	
+	//printf(" \n");	
 	return 0;
 }
