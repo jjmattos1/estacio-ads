0836c595ce181cd9f4ee2e930af49a5aa605c49b
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-10-10 21:22:36 -0300

    Atualizando repo Windows.

--- 22q3/IntroProgEstC/T6¨_VetoresEMatrizes/VM_VetorPratica1.c
+++ 22q3/IntroProgEstC/T6¨_VetoresEMatrizes/VM_VetorPratica1.c
@@ -5,10 +5,14 @@ int main ()
 {
 	printf("Vetores e Matrizes - Vetor, Pratica 1\n\n");
 	
-	int vetSize = 3;
-	int vetP1[3];
-	int repStart1, repStart2, repStart3, vetNum = 0;
+	int repStart1, repStart2, repStart3, vetNum, vetSize = 0;
+			
+		printf("Digite o tamanho do Vetor desejado: ");
+	scanf("%d", &vetSize);
+	printf("\n");
 	
+	vetSize -= 1;
+	int vetP1[vetSize];
 	for ( repStart1 = 0 ; repStart1 <= vetSize ; repStart1 ++ )
 	{
 		vetP1[repStart1] = 0;
@@ -19,8 +23,11 @@ int main ()
 		printf("Digite o numero desejado da posicao %d do Vetor: ", repStart2);
 		scanf("%d", (&vetP1[repStart2]));
 		
-		printf(" \n");
+		//printf(" \n");
 	}
+	
+	printf("\n");
+	//printf(vetSize);
 	
 	for ( repStart3 = vetSize ; repStart3 >= 0 ; repStart3 -- )
 	{	
