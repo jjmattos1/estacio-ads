987c33350f73e3cb94bbd756122bc3bc8d4f2c6d
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-10-11 09:01:10 -0300

    Atualizando repo Android.

---
+++ 22q3/IntroProgEstC/T6_VetoresEMatrizes/VM_VetorPratica2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	printf("Vetores e Matrizes - Vetor, Pratica 2\n\n");
+	printf("Armazenar a nota de 20 alunos e exibir apenas as quais estão acima da média da turma.\n\n");
+	
+	int repStart, vetSize, currentStud = 0;
+	float scoreSum, typedScore, avg = 0.0;
+			
+		printf("Digite a quantidade de alunos desejada: ");
+	scanf("%d", &vetSize);
+	printf("\n");
+	
+	vetSize -= 1;
+	float vetP2[vetSize], vetPosVar;
+	for ( repStart = 0 ; repStart <= vetSize ; repStart ++ )
+	{
+		vetP2[repStart] = 0.0;
+	}
+	
+	repStart = 0;
+	
+	for ( repStart = 0 ; repStart <= vetSize ; repStart ++ )
+	{	
+		currentStud = repStart + 1;
+		printf("Digite a nota do %do aluno: ", currentStud);
+		//vetPosVar = (float(repStart2));
+		scanf("%f", &typedScore);
+		
+		vetP2[repStart] = typedScore;
+		scoreSum += typedScore;
+		typedScore = 0.0;
+	}
+	
+	// cleaning variables content
+	repStart, currentStud = 0;
+	
+	printf("\n");
+	
+	avg = scoreSum / vetSize;
+	
+	for ( repStart = 0 ; repStart <= vetSize ; repStart ++)
+		{
+			if ( (vetP2[repStart]) >= avg )
+			{
+				currentStud = repStart + 1;
+				printf("O %do aluno esta acima da media: ", currentStud);
+				printf(" ");
+				printf("(media do aluno %f)", (vetP2[repStart]));
+				//eof if
+			}
+		}
+	
+	/*
+	for ( repStart3 = vetSize ; repStart3 >= 0 ; repStart3 -- )
+	{	
+		printf("O numero da posicao %d do vetor e: ", repStart3);
+		printf("%d", (vetP1[repStart3]));
+		printf(" \n");
+	}
+	*/
+		
+	return 0;
+}}
