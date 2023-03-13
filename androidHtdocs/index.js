4ed41cd171926fef1e7675ae282cb2afdbcf95b8
jjmattos1 <brainstorm@jonasmattos.com.br>
2023-03-12 21:27:44 -0300

    Atualizando repo Windows.

--- androidHtdocs/index.js
+++ androidHtdocs/index.js
@@ -14,6 +14,12 @@ function displayWSize() {
   showDS.innerHTML = "Browser display size is "+w+" x "+h+"!";
 }
 
+function setAudioInitVol() {
+  var audio = document.getElementById("audio1");
+  audio.volume = 0.2;
+}
+
+
 /*
 É interessante notar que as duas produzem o mesmo efeito visual (que <b> e <i> respectivamente) em um texto, ou seja, marcá-lo como negrito e/ou itálico. Entretanto, há uma diferença importante entre elas, que vai além da visualização do texto no navegador pela maioria dos usuários. A função semântica de ambas é perceptível em dispositivos de leitura de tela, que transformam o texto em áudio, e normalmente são utilizados por pessoas com deficiência visual.
 */
