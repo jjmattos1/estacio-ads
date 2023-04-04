636e6d8a12a0a82d27af873a3c879a2344fd3de9
jjmattos1 <brainstorm@jonasmattos.com.br>
2023-04-04 09:05:18 -0300

    Update on DevWebETC @ theme 5 - module 1.

---
+++ 23s1/DevWebEtc/theme05/mod01-tests.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Hello World!";
+echo "\n\n";
+$result1 = num1 ** num2;
+echo "";
+// PHP is like Javascript regarding the variable declaration, you dont define the variable types.
+/*
+The ** operator serves to exponentiate the second variable:
+$test = 10 ** 2;
+In this case the value of the test variable is 100.
+
+Request variables:
+
+$_GET = retrieve data
+$_POST = send data
+$_REQUEST = retrieve/send data
+
+
+
+*/
+?>
