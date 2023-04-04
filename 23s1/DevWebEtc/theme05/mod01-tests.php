15f1897445148cc3f722221d01751d31145a46c5
jjmattos1 <brainstorm@jonasmattos.com.br>
2023-04-04 20:05:03 -0300

    Update on DevWebETC @ theme 5 - module 1.

--- 23s1/DevWebEtc/theme05/mod01-tests.php
+++ 23s1/DevWebEtc/theme05/mod01-tests.php
@@ -1,9 +1,12 @@
 <?php
 
 echo "Hello World!";
-echo "\n\n";
-$result1 = num1 ** num2;
-echo "";
+echo "<br></br><br></br>";
+$num1 = 10;
+$num2 = 2;
+$result1 = $num1 ** $num2;
+echo "O resultado de $num1 elevado a $num2 é $result1.";
+die;
 // PHP is like Javascript regarding the variable declaration, you dont define the variable types.
 /*
 The ** operator serves to exponentiate the second variable:
