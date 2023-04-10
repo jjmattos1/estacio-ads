85a17ebe6521390be79d63aeda4cf6c15bb0eb2f
jjmattos1 <brainstorm@jonasmattos.com.br>
2023-04-10 19:57:55 -0300

    Update on DevWebETC @ theme 5.

--- 23s1/DevWebEtc/theme05/form_login.php
+++ 23s1/DevWebEtc/theme05/form_login.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Formulário de Login</title>
+  <title>Login do Usuário</title>
 </head>
 <body>
   <?php
@@ -12,7 +12,9 @@
       echo("<hr/><h2>".$_GET["erro"]."</h2><hr/>");
   ?>
   <form action="login.php" method="post">
-    <input type="text">
+    <p>Login: <input type="text" name="login" /></p>
+    <p>Password: <input type="text" name="pw" /></p>
+    <input type="submit" value="Enviar"/>
   </form>
 </body>
 </html>
